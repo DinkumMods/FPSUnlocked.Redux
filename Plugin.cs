5b5822885754ee6212a6077a0ee07691154cfeaa
VaterOtter <adamdhoward@gmail.com>
2024-01-06 13:16:39 -0500

    Initial Commit
    * Updated to Summer Sun Compatibility

---
+++ Plugin.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Core.Logging.Interpolation;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace FPSUnlock.Redux
+{
+    // Token: 0x02000003 RID: 3
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+
+        private void StoreModId()
+        {
+            ConfigEntry<int> configEntry = base.Config.Bind<int>("Developer", "NexusID", PluginInfo.nexusId, "nexus mod id -- automatically generated -- cannot be changed");
+            if (configEntry.Value != 312)
+            {
+                configEntry.Value = 312;
+                base.Config.Save();
+            }
+        }
+        // Token: 0x06000001 RID: 1 RVA: 0x00002050 File Offset: 0x00000250
+        private void Awake()
+        {
+            Logger.LogInfo(Plugin.Log);
+            ManualLogSource log = Plugin.Log;
+            bool flag;
+            BepInExInfoLogInterpolatedStringHandler bepInExInfoLogInterpolatedStringHandler = new BepInExInfoLogInterpolatedStringHandler(14, 0, out flag);
+            if (flag)
+            {
+                bepInExInfoLogInterpolatedStringHandler.AppendLiteral("Plugin loaded!");
+            }
+            log.LogInfo(bepInExInfoLogInterpolatedStringHandler);
+        }
+
+        // Token: 0x06000002 RID: 2 RVA: 0x00002098 File Offset: 0x00000298
+        private void Start()
+        {
+            this.StoreModId();
+            Application.targetFrameRate = -1;
+            QualitySettings.vSyncCount = 0;
+            ManualLogSource logger = base.Logger;
+            bool flag;
+            BepInExInfoLogInterpolatedStringHandler bepInExInfoLogInterpolatedStringHandler = new BepInExInfoLogInterpolatedStringHandler(20, 0, out flag);
+            if (flag)
+            {
+                bepInExInfoLogInterpolatedStringHandler.AppendLiteral("Target Fps set to -1");
+            }
+            logger.LogInfo(bepInExInfoLogInterpolatedStringHandler);
+        }
+
+        // Token: 0x04000004 RID: 4
+        internal static ManualLogSource Log = new ManualLogSource("FPSUnlock.Redux");
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FPSUnlock.Redux
+{
+    // Token: 0x02000002 RID: 2
+    public static class PluginInfo
+    {
+        // Token: 0x04000001 RID: 1
+        public const string PLUGIN_GUID = "FPSUnlock.Redux";
+
+        // Token: 0x04000002 RID: 2
+        public const string PLUGIN_NAME = "FPSUnlock.Redux";
+
+        // Token: 0x04000003 RID: 3
+        public const string PLUGIN_VERSION = "1.0.0";
+
+        public const int nexusId = 315;
+    }
+}
